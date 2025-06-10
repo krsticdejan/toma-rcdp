bf9e645c66caa416465b84e61f2480276b1ac28a
Dejan <94181859+krsticdejan@users.noreply.github.com>
2025-06-10 22:25:05 +0200

    fix smooth scroll

--- src/app/components/home/SmoothWrapper.jsx
+++ src/app/components/home/SmoothWrapper.jsx
@@ -1,44 +1,42 @@
 "use client";
 
-import { useRef, useEffect } from "react";
-import { gsap } from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import ScrollSmoother from "gsap/ScrollSmoother";
-import { usePathname } from "next/navigation";
+// import { useRef, useEffect } from "react";
+// import { gsap } from "gsap";
+// import ScrollTrigger from "gsap/ScrollTrigger";
+// import ScrollSmoother from "gsap/ScrollSmoother";
+// import { usePathname } from "next/navigation";
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 export default function SmoothWrapper({ children }) {
-  const wrapperRef = useRef(null);
-  const contentRef = useRef(null);
-  const pathname = usePathname(); // track route changes
+  // const wrapperRef = useRef(null);
+  // const contentRef = useRef(null);
+  // const pathname = usePathname(); // track route changes
 
-  useEffect(() => {
-    let smoother;
+  // useEffect(() => {
+  //   let smoother;
 
-    if (typeof window !== "undefined") {
-      // Kill all existing smoothers (in case it's not properly destroyed)
+  //   if (typeof window !== "undefined") {
+  //     // Kill all existing smoothers (in case it's not properly destroyed)
 
-      smoother = ScrollSmoother.create({
-        wrapper: wrapperRef.current,
-        content: contentRef.current,
-        smooth: 1.2,
-        normalizeScroll: true,
-        ignoreMobileResize: true,
-        preventDefault: true,
-      });
-    }
+  //     smoother = ScrollSmoother.create({
+  //       wrapper: wrapperRef.current,
+  //       content: contentRef.current,
+  //       smooth: 1.2,
+  //       normalizeScroll: true,
+  //       ignoreMobileResize: true,
+  //       preventDefault: true,
+  //     });
+  //   }
 
-    return () => {
-      if (smoother) smoother.kill();
-    };
-  }, [pathname]); // re-run on route change
+  //   return () => {
+  //     if (smoother) smoother.kill();
+  //   };
+  // }, [pathname]); // re-run on route change
 
   return (
-    <div id="smooth-wrapper" ref={wrapperRef}>
-      <div id="smooth-content" ref={contentRef}>
-        {children}
-      </div>
+    <div id="smooth-wrapper">
+      <div id="smooth-content">{children}</div>
     </div>
   );
 }
