64aa073ec6c31d750489178a63cdd73056b1e917
Dejan <94181859+krsticdejan@users.noreply.github.com>
2025-07-06 15:37:54 +0200

    experimental transition

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    viewTransition: true,
-  },
+  // experimental: {
+  //   viewTransition: true,
+  // },
   images: {
     remotePatterns: [
       {
